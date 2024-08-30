206dc40930e6261f0f425a4b05e65cafdf9adb73
mhconradt <mhconradt@protonmail.com>
2024-08-30 16:43:48 -0700

    Turn it into a Cargo package

--- codebased/constants.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-DEFAULT_MODEL = 'text-embedding-3-large'
-DEFAULT_MODEL_DIMENSIONS = 256
-EMBEDDING_MODEL_CONTEXT_LENGTH = 8192
-DEFAULT_EDITOR = "vi"
--- codebased/utils.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-import hashlib
-
-
-def get_content_hash(content: bytes) -> str:
-    return hashlib.sha1(content).hexdigest()
