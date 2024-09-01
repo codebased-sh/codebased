b4bbdaf345150a7d0162762ae482f0d549e5b6cc
mhconradt <mhconradt@protonmail.com>
2024-09-01 16:25:19 -0700

    Slightly clean up main

--- codebased/main.py
+++ codebased/main.py
@@ -1,11 +1,25 @@
 import argparse
 import sys
+import typing as T
 from pathlib import Path
 
 VERSION = "0.0.1"
 
 
-def exit_with_error(message: str, *, exit_code: int = 1):
+def find_root_git_repository(path: Path):
+    # copy to mutate
+    search_current_dir = Path(path).resolve()
+    search_result_dir = None
+    do_while_cond = True
+    while do_while_cond:
+        if (search_current_dir / '.git').is_dir():
+            search_result_dir = search_current_dir
+        search_current_dir = search_current_dir.parent.resolve()
+        do_while_cond = search_current_dir != Path('/')
+    return search_result_dir
+
+
+def exit_with_error(message: str, *, exit_code: int = 1) -> T.NoReturn:
     print(message, file=sys.stderr)
     sys.exit(exit_code)
 
@@ -40,23 +54,11 @@ def main():
     args = parser.parse_args()
 
     if args.command == 'search':
-        search_result_dir = find_root_git_repository(args.directory)
-        if search_result_dir is None:
+        git_repository = find_root_git_repository(args.directory)
+        if git_repository is None:
             exit_with_error('Codebased must be run within a Git repository.')
-        print(f'Found Git repository {search_result_dir}')
-
-
-def find_root_git_repository(path: Path):
-    # copy to mutate
-    search_current_dir = Path(path).resolve()
-    search_result_dir = None
-    do_while_cond = True
-    while do_while_cond:
-        if (search_current_dir / '.git').is_dir():
-            search_result_dir = search_current_dir
-        search_current_dir = search_current_dir.parent.resolve()
-        do_while_cond = search_current_dir != Path('/')
-    return search_result_dir
+        git_repository: Path = git_repository
+        print(f'Found Git repository {git_repository}')
 
 
 if __name__ == '__main__':
