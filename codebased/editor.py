762b84a1524fc3ee360a977ba2c93b831a689fd4
mhconradt <mhconradt@protonmail.com>
2024-09-03 20:40:25 -0700

    Fix line numbers

--- codebased/editor.py
+++ codebased/editor.py
@@ -8,11 +8,12 @@ def suspends(editor: Literal["vi", "idea", "code"]) -> bool:
 
 
 def open_editor(editor: Literal["vi", "idea", "code"], *, file: Path, row: int, column: int):
+    line_number = row + 1
     if editor == "vi":
-        subprocess.run(["vi", str(file), f"+{row}"])
+        subprocess.run(["vi", str(file), f"+{line_number}"])
     elif editor == "idea":
-        subprocess.run(["idea", "--line", str(row), str(file)])
+        subprocess.run(["idea", "--line", str(line_number), str(file)])
     elif editor == "code":
-        subprocess.run(["code", "--goto", f"{file}:{row}:{column}"])
+        subprocess.run(["code", "--goto", f"{file}:{line_number}:{column}"])
     else:
         raise NotImplementedError(editor)
