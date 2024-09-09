1d6f280afcf70ee762af7a97f7a385e013f88a19
mhconradt <mhconradt@protonmail.com>
2024-09-08 18:51:49 -0700

    Release

--- codebased/main.py
+++ codebased/main.py
@@ -15,7 +15,7 @@ from codebased.settings import Settings
 from codebased.stats import STATS
 from codebased.tui import Codebased
 
-VERSION = "0.4.8"
+VERSION = "0.4.9"
 
 cli = typer.Typer(
     name="Codebased CLI",
